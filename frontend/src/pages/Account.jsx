da2ec4b9cae54a1fa1864a13c2b29be39f6ae735
CrisMachuca <cristinamachucam@gmail.com>
2024-10-02 23:50:25 +0200

    actualizo account que no mostraba datos

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -3,19 +3,27 @@ import API from '../api';
 
 function Account() {
     const [userData, setUserdata] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchUserData = async () => {
           const token = localStorage.getItem('token');
-          try {
-            const response = await API.get('/account', {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            });
-            setUserData(response.data);
-          } catch (error) {
-            console.error('Error fetching account data', error);
+          if (token) {
+            try {
+              const response = await API.get('/account', {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              setUserdata(response.data);
+            } catch (error) {
+              console.error('Error fetching account data', error);
+            } finally {
+              setLoading(false);
+            } 
+          } else {
+            console.log('Notoken found');
+            setLoading(false);
           }
         };
     
@@ -25,13 +33,15 @@ function Account() {
       return (
         <div className="container mx-auto mt-8">
             <h1 className="text-3xl font-bold mb-4">Mi Cuenta</h1>
-            {userData ? (
-            <div>
-                <p><strong>Username:</strong> {userData.username}</p>
-                <p><strong>Email:</strong> {userData.email}</p>
-            </div>
+            {loading ? (
+                <p>Cargando datos de usuario...</p>
+            ) : userData ? (
+                <div>
+                    <p><strong>Username:</strong> {userData.username}</p>
+                    <p><strong>Email:</strong> {userData.email}</p>
+                </div>
             ) : (
-            <p>Cargando datos de usuario...</p>
+                <p>No se pudieron cargar los datos del usuario.</p>
             )}
         </div>
       )
