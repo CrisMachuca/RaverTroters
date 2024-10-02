710c5e53ba69bcae092c0bf6b6a8b7127ba1156a
CrisMachuca <cristinamachucam@gmail.com>
2024-10-03 01:10:17 +0200

    corrijo fallo agregar producto desde admin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-2xl font-bold">RaverTroter</Link>
+        <Link to="/" className="text-white text-2xl font-bold">RaverTroters</Link>
         <div>
           <Link to="/" className="text-white mr-4">Inicio</Link>
           <Link to="/products" className="text-white mr-4">Productos</Link>
